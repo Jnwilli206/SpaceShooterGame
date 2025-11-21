37647de3b6f7f92768fe476dc9bf6b11381f4495
Joey Williams <jnwilliams206@yahoo.com>
2025-11-21 13:35:34 -0500

    Build Settings and Fixes
    Settings for building the game so far

--- Assets/DiagEnemy.cs
+++ Assets/DiagEnemy.cs
@@ -3,23 +3,30 @@ using UnityEngine;
 public class DiagEnemy : MonoBehaviour
 {
     [SerializeField] float diagSpeed;    
-    
+    private bool leftRight;
     
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if (transform.position.x < 0) 
+        {
+            leftRight = true;
+        }
+        if (transform.position.x > 0) 
+        {
+            leftRight = false;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < 0) 
+        if (leftRight == true) 
         {
             transform.position -= new Vector3(-2f, 0, 0) * Time.deltaTime;
         }
-        if (transform.position.x > 0) 
+        if (leftRight == false) 
         {
             transform.position -= new Vector3(2f, 0, 0) * Time.deltaTime;
         }
