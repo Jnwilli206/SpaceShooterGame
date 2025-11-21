9a568bf6e8a12aaddcca1dc7f5e2bd1e49c9ee47
Joey Williams <jnwilliams206@yahoo.com>
2025-11-20 21:33:30 -0500

    DiagFighter and OrbEnemy collider fix
    Added a new enemy and fixed the orb one.

--- Assets/Scripts/OrbEnemy.cs
+++ Assets/Scripts/OrbEnemy.cs
@@ -8,15 +8,20 @@ public class OrbEnemy : MonoBehaviour
     public Sprite[] sprites; // Assign your sprites in the Inspector
     private int currentSpriteIndex = 0;
     [SerializeField] float seconds;
+    private CircleCollider2D circleCollider;
+
 
     void Start()
     {
         // Get the SpriteRenderer component if not assigned in Inspector
+        circleCollider = GetComponent<CircleCollider2D>();
         if (spriteRenderer == null)
         {
-            spriteRenderer = GetComponent<SpriteRenderer>();
+            spriteRenderer = GetComponentInChildren<SpriteRenderer>();
         }
         StartCoroutine(ChangeSpriteRoutine());
+
+
     }
 
     IEnumerator ChangeSpriteRoutine()
@@ -25,8 +30,19 @@ public class OrbEnemy : MonoBehaviour
         {
             spriteRenderer.sprite = sprites[currentSpriteIndex];
             currentSpriteIndex = (currentSpriteIndex + 1) % sprites.Length; // Cycle through sprites
-            yield return new WaitForSeconds(seconds); 
+
+            if (currentSpriteIndex == 3 || currentSpriteIndex == 4)
+            {
+                circleCollider.radius = (0.65f);
+            }
+            else
+            {
+                circleCollider.radius = 0.25f;
+            }
+        
+
+            yield return new WaitForSeconds(seconds);
+
         }
     }
 }
-
